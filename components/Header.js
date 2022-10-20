0dccf33dbc4fcd4c894f35bf449ea30e9f96f714
saurabhdhebe2 <108879055+saurabhdhebe2@users.noreply.github.com>
2022-10-20 19:02:21 +0530

    added navbar

--- components/Header.js
+++ components/Header.js
@@ -6,8 +6,10 @@ import {
 } from "@heroicons/react/24/solid";
 import Image from "next/image";
 import HeaderIcon from "./HeaderIcon";
+import { useRouter } from "next/router";
 
 const Header = () => {
+  const router = useRouter();
   return (
     <div className="bg-gray-700 text-gray-200 flex flex-col items-center p-6 select-none sm:flex-row justify-between">
       <div className="flex ">
@@ -17,6 +19,7 @@ const Header = () => {
         <HeaderIcon Icon={InformationCircleIcon} title="ABOUT" />
       </div>
       <Image
+        onClick={() => router.push("/")}
         className="cursor-pointer active:brightness-110"
         src="https://upload.wikimedia.org/wikipedia/commons/6/69/IMDB_Logo_2016.svg"
         width={100}
