c4e0238a8c5f0f279a1de0498f5a3da52e4dd00a
saurabhdhebe2 <108879055+saurabhdhebe2@users.noreply.github.com>
2022-10-20 17:52:22 +0530

    created header component

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,26 @@
+import {
+  HomeIcon,
+  UserIcon,
+  PhoneIcon,
+  InformationCircleIcon,
+} from "@heroicons/react/24/solid";
+import Image from "next/image";
+import HeaderIcon from "./HeaderIcon";
+
 const Header = () => {
   return (
     <div>
-      <h1>Header</h1>
+      <div className="">
+        <HeaderIcon Icon={HomeIcon} title="HOME" />
+        <HeaderIcon Icon={UserIcon} title="ACCOUNT" />
+        <HeaderIcon Icon={PhoneIcon} title="CONTACT" />
+        <HeaderIcon Icon={InformationCircleIcon} title="ABOUT" />
+      </div>
+      <Image
+        src="https://upload.wikimedia.org/wikipedia/commons/6/69/IMDB_Logo_2016.svg"
+        width={100}
+        height={100}
+      />
     </div>
   );
 };
---
+++ components/HeaderIcon.js
@@ -0,0 +1,10 @@
+const HeaderIcon = ({ Icon, title }) => {
+  return (
+    <div>
+      <Icon className="h-8" />
+      <p>{title}</p>
+    </div>
+  );
+};
+
+export default HeaderIcon;
