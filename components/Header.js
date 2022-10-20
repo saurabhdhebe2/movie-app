afbd0a3609094c45b69e5b0ee71f31c7819c8134
saurabhdhebe2 <108879055+saurabhdhebe2@users.noreply.github.com>
2022-10-20 18:31:03 +0530

    added style to header component

--- components/Header.js
+++ components/Header.js
@@ -9,14 +9,15 @@ import HeaderIcon from "./HeaderIcon";
 
 const Header = () => {
   return (
-    <div>
-      <div className="">
+    <div className="bg-gray-700 text-gray-200 flex flex-col items-center p-6 select-none sm:flex-row justify-between">
+      <div className="flex ">
         <HeaderIcon Icon={HomeIcon} title="HOME" />
         <HeaderIcon Icon={UserIcon} title="ACCOUNT" />
         <HeaderIcon Icon={PhoneIcon} title="CONTACT" />
         <HeaderIcon Icon={InformationCircleIcon} title="ABOUT" />
       </div>
       <Image
+        className="cursor-pointer active:brightness-110"
         src="https://upload.wikimedia.org/wikipedia/commons/6/69/IMDB_Logo_2016.svg"
         width={100}
         height={100}
